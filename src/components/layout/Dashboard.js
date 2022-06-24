b1887e7dc062e4391bfaafd8e603f234cd8cd020
anuragroy17 <anuragroy17@gmail.com>
2022-06-24 17:32:17 +0530

    delete modal

--- src/components/layout/Dashboard.js
+++ src/components/layout/Dashboard.js
@@ -13,6 +13,7 @@ import { useDataLayerValue } from '../../context-api/Datalayer';
 import { actionTypes } from '../../context-api/reducer';
 import { deleteMultiple } from '../../firebase';
 import { DrawerHeader } from '../../shared/ui-themes';
+import AlertDialog from '../UI/AlertDialog';
 import AddNote from './notes/AddNote';
 import Note from './notes/Note';
 
@@ -37,6 +38,7 @@ const Dashboard = (props) => {
   const [title, setTitle] = useState('');
   const [note, setNote] = useState('');
   const [id, setId] = useState('');
+  const [prompt, setPrompt] = useState(false);
   const [{ isLoading }, dispatch] = useDataLayerValue();
 
   const handleEdit = () => {
@@ -85,103 +87,124 @@ const Dashboard = (props) => {
     props.getTrashed();
   };
 
+  const openDialog = () => {
+    setPrompt(true);
+  };
+
+  const closeDialog = () => {
+    setPrompt(false);
+  };
+
+  const agreeDelete = () => {
+    deleteAll();
+    setPrompt(false);
+  };
+
   return (
-    <Box
-      sx={{
-        flexGrow: 1,
-        p: 3,
-        overflowX: 'hidden',
-      }}
-    >
-      <DrawerHeader />
-      {props.showAddNote && (
-        <AddNote
-          edit={edit}
-          isUpdate={isUpdate}
-          title={title}
-          note={note}
-          id={id}
-          uid={props.uid}
-          onAdd={handleAdd}
-          handleEdit={handleEdit}
-          closeEdit={closeEdit}
-        />
-      )}
-
-      {props.showDeleteAll && props.notes.length !== 0 && (
-        <Tooltip title="Delete All">
-          <Fab
-            aria-label="delete-all"
-            onClick={deleteAll}
-            sx={{ position: 'fixed', right: 15, bottom: 15 }}
-          >
-            <RemoveCircleOutlineIcon />
-          </Fab>
-        </Tooltip>
-      )}
-
-      {props.notes.length === 0 && (
-        <Card
-          variant="outlined"
-          sx={{
-            maxWidth: 400,
-            minWidth: 200,
-            margin: '0 auto',
-            borderRadius: '20px',
-          }}
-        >
-          <CardMedia
-            component="img"
-            height="200"
-            sx={{
-              display: 'block',
-              marginLeft: 'auto',
-              marginRight: 'auto',
-              width: '50%',
-              objectFit: 'contain',
-            }}
-            image="images/no_data.svg"
-            alt="no data"
-          />
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="div">
-              No Notes Found!
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
-              Please add notes from Notes section
-            </Typography>
-          </CardContent>
-        </Card>
-      )}
-
+    <>
+      <AlertDialog
+        open={prompt}
+        agree={agreeDelete}
+        close={closeDialog}
+        isSingle={false}
+      />
       <Box
         sx={{
-          display: 'grid',
-          gridTemplateColumns: 'repeat(auto-fill,minmax(auto,220px))',
-          justifyContent: 'space-evenly',
-          gridColumnGap: '10px',
+          flexGrow: 1,
+          p: 3,
+          overflowX: 'hidden',
         }}
       >
-        {props.notes.length !== 0 &&
-          props.notes.map((note) => (
-            <Note
-              key={note.id}
-              id={note.id}
-              uid={props.uid}
-              title={note.title}
-              note={note.note}
-              isNote={note.isNote}
-              isArchived={note.isArchived}
-              isTrashed={note.isTrashed}
-              createdDate={`${
-                months[note.date.getMonth()]
-              } ${note.date.getDate()}, ${note.date.getFullYear()}`}
-              onClick={handleOnCLick}
-              editNote={editNote}
+        <DrawerHeader />
+        {props.showAddNote && (
+          <AddNote
+            edit={edit}
+            isUpdate={isUpdate}
+            title={title}
+            note={note}
+            id={id}
+            uid={props.uid}
+            onAdd={handleAdd}
+            handleEdit={handleEdit}
+            closeEdit={closeEdit}
+          />
+        )}
+
+        {props.showDeleteAll && props.notes.length !== 0 && (
+          <Tooltip title="Delete All">
+            <Fab
+              aria-label="delete-all"
+              onClick={openDialog}
+              sx={{ position: 'fixed', right: 15, bottom: 15 }}
+            >
+              <RemoveCircleOutlineIcon />
+            </Fab>
+          </Tooltip>
+        )}
+
+        {props.notes.length === 0 && (
+          <Card
+            variant="outlined"
+            sx={{
+              maxWidth: 400,
+              minWidth: 200,
+              margin: '0 auto',
+              borderRadius: '20px',
+            }}
+          >
+            <CardMedia
+              component="img"
+              height="200"
+              sx={{
+                display: 'block',
+                marginLeft: 'auto',
+                marginRight: 'auto',
+                width: '50%',
+                objectFit: 'contain',
+              }}
+              image="images/no_data.svg"
+              alt="no data"
             />
-          ))}
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                No Notes Found!
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                Please add notes from Notes section
+              </Typography>
+            </CardContent>
+          </Card>
+        )}
+
+        <Box
+          sx={{
+            display: 'grid',
+            gridTemplateColumns: 'repeat(auto-fill,minmax(auto,220px))',
+            justifyContent: 'space-evenly',
+            gridColumnGap: '10px',
+          }}
+        >
+          {props.notes.length !== 0 &&
+            props.notes.map((note) => (
+              <Note
+                key={note.id}
+                id={note.id}
+                uid={props.uid}
+                title={note.title}
+                note={note.note}
+                isNote={note.isNote}
+                isArchived={note.isArchived}
+                isTrashed={note.isTrashed}
+                createdDate={`${
+                  months[note.date.getMonth()]
+                } ${note.date.getDate()}, ${note.date.getFullYear()}`}
+                onClick={handleOnCLick}
+                editNote={editNote}
+              />
+            ))}
+        </Box>
       </Box>
-    </Box>
+    </>
   );
 };
 
--- src/components/layout/notes/Note.js
+++ src/components/layout/notes/Note.js
@@ -15,9 +15,11 @@ import UnarchiveIcon from '@mui/icons-material/Unarchive';
 import { setLocation, deleteNote } from '../../../firebase';
 import { useDataLayerValue } from '../../../context-api/Datalayer';
 import { actionTypes } from '../../../context-api/reducer';
+import AlertDialog from '../../UI/AlertDialog';
 
 const Note = (props) => {
   const [show, setShow] = useState(false);
+  const [prompt, setPrompt] = useState(false);
   const [{ isLoading }, dispatch] = useDataLayerValue();
 
   const setLoader = (isLoading) => {
@@ -51,110 +53,131 @@ const Note = (props) => {
     props.editNote(props.title, props.note, props.id, props.uid);
   };
 
-  return (
-    <Card
-      variant="outlined"
-      onMouseOver={() => setShow(true)}
-      onMouseOut={() => setShow(false)}
-      sx={{ width: 200, borderRadius: '20px', marginBottom: '10px' }}
-    >
-      <CardHeader
-        title={
-          <Tooltip title={props.title}>
-            <Typography noWrap gutterBottom variant="h6" component="h4">
-              {props.title}
-            </Typography>
-          </Tooltip>
-        }
-        subheader={props.createdDate}
-        sx={{ display: 'block', overflow: 'hidden' }}
-      />
-      <CardContent sx={{ paddingBottom: '3px' }}>
-        <Typography
-          variant="body2"
-          color="text.secondary"
-          sx={{ overflowWrap: 'break-word' }}
-        >
-          {props.note}
-        </Typography>
-      </CardContent>
-      <CardActions
-        disableSpacing
-        sx={{
-          display: 'flex',
-          justifyContent: 'right',
-          paddingTop: 0,
-        }}
-      >
-        {props.isNote && (
-          <Tooltip title="Edit">
-            <IconButton
-              aria-label="edit"
-              sx={{ visibility: show ? 'visible' : 'hidden' }}
-              onClick={editNote}
-            >
-              <Edit />
-            </IconButton>
-          </Tooltip>
-        )}
+  const openDialog = () => {
+    setPrompt(true);
+  };
 
-        {(props.isNote || props.isArchived) && (
-          <Tooltip title="Archive">
-            <IconButton
-              aria-label="archive"
-              sx={{ visibility: show ? 'visible' : 'hidden' }}
-              onClick={() => setLocationinFS('isArchived', true)}
-            >
-              <Archive />
-            </IconButton>
-          </Tooltip>
-        )}
-        {props.isArchived && (
-          <Tooltip title="Unarchive">
-            <IconButton
-              aria-label="archive"
-              sx={{ visibility: show ? 'visible' : 'hidden' }}
-              onClick={() => setLocationinFS('isNote', true)}
-            >
-              <UnarchiveIcon />
-            </IconButton>
-          </Tooltip>
-        )}
-        {props.isTrashed && (
-          <Tooltip title="Restore">
-            <IconButton
-              aria-label="delete"
-              sx={{ visibility: show ? 'visible' : 'hidden' }}
-              onClick={() => setLocationinFS('isNote', true)}
-            >
-              <RestoreFromTrashIcon />
-            </IconButton>
-          </Tooltip>
-        )}
-        {props.isTrashed && (
-          <Tooltip title="Delete Forever">
-            <IconButton
-              aria-label="delete"
-              sx={{ visibility: show ? 'visible' : 'hidden' }}
-              onClick={deleteNoteFromFS}
-            >
-              <DeleteForeverIcon />
-            </IconButton>
-          </Tooltip>
-        )}
-        {(props.isNote || props.isArchived) && (
-          <Tooltip title="Delete">
-            <IconButton
-              aria-label="delete"
-              sx={{ visibility: show ? 'visible' : 'hidden' }}
-              onClick={() => setLocationinFS('isTrashed', true)}
-            >
-              <Delete />
-            </IconButton>
-          </Tooltip>
-        )}
-      </CardActions>
-    </Card>
+  const closeDialog = () => {
+    setPrompt(false);
+  };
+
+  const agreeDelete = () => {
+    deleteNoteFromFS();
+    setPrompt(false);
+  };
+
+  return (
+    <>
+      <AlertDialog
+        open={prompt}
+        agree={agreeDelete}
+        close={closeDialog}
+        isSingle={true}
+      />
+      <Card
+        variant="outlined"
+        onMouseOver={() => setShow(true)}
+        onMouseOut={() => setShow(false)}
+        sx={{ width: 200, borderRadius: '20px', marginBottom: '10px' }}
+      >
+        <CardHeader
+          title={
+            <Tooltip title={props.title}>
+              <Typography noWrap gutterBottom variant="h6" component="h4">
+                {props.title}
+              </Typography>
+            </Tooltip>
+          }
+          subheader={props.createdDate}
+          sx={{ display: 'block', overflow: 'hidden' }}
+        />
+        <CardContent sx={{ paddingBottom: '3px' }}>
+          <Typography
+            variant="body2"
+            color="text.secondary"
+            sx={{ overflowWrap: 'break-word' }}
+          >
+            {props.note}
+          </Typography>
+        </CardContent>
+        <CardActions
+          disableSpacing
+          sx={{
+            display: 'flex',
+            justifyContent: 'right',
+            paddingTop: 0,
+          }}
+        >
+          {props.isNote && (
+            <Tooltip title="Edit">
+              <IconButton
+                aria-label="edit"
+                sx={{ visibility: show ? 'visible' : 'hidden' }}
+                onClick={editNote}
+              >
+                <Edit />
+              </IconButton>
+            </Tooltip>
+          )}
+
+          {(props.isNote || props.isArchived) && (
+            <Tooltip title="Archive">
+              <IconButton
+                aria-label="archive"
+                sx={{ visibility: show ? 'visible' : 'hidden' }}
+                onClick={() => setLocationinFS('isArchived', true)}
+              >
+                <Archive />
+              </IconButton>
+            </Tooltip>
+          )}
+          {props.isArchived && (
+            <Tooltip title="Unarchive">
+              <IconButton
+                aria-label="archive"
+                sx={{ visibility: show ? 'visible' : 'hidden' }}
+                onClick={() => setLocationinFS('isNote', true)}
+              >
+                <UnarchiveIcon />
+              </IconButton>
+            </Tooltip>
+          )}
+          {props.isTrashed && (
+            <Tooltip title="Restore">
+              <IconButton
+                aria-label="delete"
+                sx={{ visibility: show ? 'visible' : 'hidden' }}
+                onClick={() => setLocationinFS('isNote', true)}
+              >
+                <RestoreFromTrashIcon />
+              </IconButton>
+            </Tooltip>
+          )}
+          {props.isTrashed && (
+            <Tooltip title="Delete Forever">
+              <IconButton
+                aria-label="delete"
+                sx={{ visibility: show ? 'visible' : 'hidden' }}
+                onClick={openDialog}
+              >
+                <DeleteForeverIcon />
+              </IconButton>
+            </Tooltip>
+          )}
+          {(props.isNote || props.isArchived) && (
+            <Tooltip title="Delete">
+              <IconButton
+                aria-label="delete"
+                sx={{ visibility: show ? 'visible' : 'hidden' }}
+                onClick={() => setLocationinFS('isTrashed', true)}
+              >
+                <Delete />
+              </IconButton>
+            </Tooltip>
+          )}
+        </CardActions>
+      </Card>
+    </>
   );
 };
 
